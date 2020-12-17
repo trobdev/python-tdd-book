7f4289bb5b84c2cacc67060c5b5a54c4c6fc91fd
trobdev <trobdev@trobsec.com>
2020-12-16 21:22:52 -0800

    functional test now checks we can input a to-do item

--- functional_tests.py
+++ functional_tests.py
@@ -17,26 +17,26 @@ class NewVisitorTest(unittest.TestCase):
 
 		#Sam notices the page title and header mentioning to-do lists
 		self.assertIn('To-Do', self.browser.title)
-		# header_text = self.browser.find_element_by_tag_name('h1').text
-		# self.assertIn('To-Do', header_text)
+		header_text = self.browser.find_element_by_tag_name('h1').text
+		self.assertIn('To-Do', header_text)
 		
 		#she is invited to enter a to-do item right away
-		# inputbox = self.browser.find_element_by_id('id_new_item')
-		# self.assertEqual(
-		# 	inputbox.get_attribute('placeholder'),
-		# 	'Enter a to-do item'
-		# )
+		inputbox = self.browser.find_element_by_id('id_new_item')
+		self.assertEqual(
+			inputbox.get_attribute('placeholder'),
+			'Enter a to-do item'
+		)
 		#she types "Buy peacock feathers" into a text box (her hobby is tying fly-fishing lures)
-		# inputbox.send_keys('Buy peacock feathers')
+		inputbox.send_keys('Buy peacock feathers')
 
 		#When she hits enter, the page updates, and now the page lists 1: Buy peacock feathers
-		# inputbox.send_keys(Keys.ENTER)
-		# time.sleep(1)
-		# table = self.browser.find_element_by_id('id_list_table')
-		# rows = table.find_eleements_by_tag_name('tr')
-		# self.assertTrue(
-		# 	any(row.text == '1: Buy peacock feathers' for row in rows)
-		# )
+		inputbox.send_keys(Keys.ENTER)
+		time.sleep(1)
+		table = self.browser.find_element_by_id('id_list_table')
+		rows = table.find_eleements_by_tag_name('tr')
+		self.assertTrue(
+			any(row.text == '1: Buy peacock feathers' for row in rows)
+		)
 		#There is still a text box inviting her to add another item, she enters "Use peacock feathers to make a fly"
 		self.fail('Finish the test!')
 		#The page update again with both items on the list
