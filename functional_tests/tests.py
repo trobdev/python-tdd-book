e6616d58df3603160513d427960884f73d12e631
trobdev <trobdev@trobsec.com>
2021-01-03 22:38:26 -0800

    eliminated hardcoded sleeps and replaced with method in functional tests

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,7 +1,10 @@
 from django.test import LiveServerTestCase
 from selenium import webdriver 
 from selenium.webdriver.common.keys import Keys
+from selenium.common.exceptions import WebDriverException
 import time
+
+MAX_WAIT = 10
 
 class NewVisitorTest(LiveServerTestCase):
 	
@@ -36,18 +39,29 @@ class NewVisitorTest(LiveServerTestCase):
 
 		#When she hits enter, the page updates, and now the page lists 1: Buy peacock feathers
 		inputbox.send_keys(Keys.ENTER)
-		time.sleep(1)
-		self.check_for_row_in_list_table('1: Buy peacock feathers')
+		self.wait_for_row_in_list_table('1: Buy peacock feathers')
 		#There is still a text box inviting her to add another item, she enters "Use peacock feathers to make a fly"
 		inputbox = self.browser.find_element_by_id('id_new_item')
 		inputbox.send_keys('Use peacock feathers to make a fly')
 		inputbox.send_keys(Keys.ENTER)
-		time.sleep(1)
 		#The page update again with both items on the list
-		self.check_for_row_in_list_table('1: Buy peacock feathers')
-		self.check_for_row_in_list_table('2: Use peacock feathers to make a fly')
+		self.wait_for_row_in_list_table('2: Use peacock feathers to make a fly')
+		self.wait_for_row_in_list_table('1: Buy peacock feathers')
 		#She wonders whetherthe site will remember her list, she sees it generates a unique URL for her and there is some explanatory text
 		self.fail('Finish the test!')
 		#She visits the URL and her to-do list is still there
 
 		#Satisfied, Sam goes to sleep
+
+	def wait_for_row_in_list_table(self, row_text):
+		start_time = time.time()
+		while True:
+			try:
+				table = self.browser.find_element_by_id('id_list_table')
+				rows = table.find_elements_by_tag_name('tr')
+				self.assertIn(row_text, [row.text for row in rows])
+				return
+			except (AssertionError, WebDriverException) as e:
+				if time.time() - start_time > MAX_WAIT:
+					raise e
+				time.sleep(0.5)
