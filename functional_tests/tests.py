6d23d3c0020ef3268ed2810c7bce187ccd4e85dc
trobdev <trobdev@trobsec.com>
2021-01-24 19:03:31 -0800

    new functional test for multiple users added

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -52,6 +52,56 @@ class NewVisitorTest(LiveServerTestCase):
 		#She visits the URL and her to-do list is still there
 
 		#Satisfied, Sam goes to sleep
+	def test_can_start_a_list_for_one_user(self):
+		# Sam has heard about a cool new online to-do app...
+		#The page updates again, and now shows both items on her list
+		self.wait_for_row_in_list_table('2: Use peacock feathers to make a fly')
+		self.wait_for_row_in_list_table('1: Buy peacock feathers')
+		# satisfied she goes back to sleep
+
+	def test_multiple_users_can_start_lists_at_different_urls(self):
+		# Sam starts a new to-do list
+		self.browser.get(self.live_server_url)
+		inputbox = self.browser.find_element_by_id('id_new_item')
+		inputbox.send_keys('Buy peacock feathers')
+		inputbox.send_keys(Keys.ENTER)
+		self.wait_for_row_in_list_table('1: Buy peacock feathers')
+		# She notices that her list has a unique URL
+		sam_list_url = self.browser.current_url
+		self.assertRegex(sam_list_url, '/lists/.+')
+
+		#now a new user, Francis, comes along to the site
+
+		##we use a new browser session to make sure that no info
+		##of Sam's is coming through from cookies etc
+		self.browser.quit()
+		self.browser = webdriver.Firefox()
+
+		#Francis visits the home page.  There is no sign of Sam's list
+		self.browser.get(self.live_server_url)
+		page_text = self.browser.find_element_by_tag_name('body').text
+		self.assertNotIn('Buy peacock feathers', page_text)
+		self.assertNotIn('make a fly', page_text)
+
+		#Francis starts a new list by entering a new item, he is less interesting than Sam
+		inputbox = self.browser.find_element_by_id('id_new_item')
+		inputbox.send_keys('Buy milk')
+		inputbox.send_keys(Keys.ENTER)
+		self.wait_for_row_in_list_table('1: Buy milk')
+
+		#Francis gets his own unique URL
+		francis_list_url = self.browser.current_url
+		self.assertRegex(francis_list_url, '/lists/.+')
+		self.assertNotEqual(francis_list_url, sam_list_url)
+
+		#Again, there is no trace of Sam's list
+		page_text = self.browser.find_element_by_tag_name('body').text
+		self.assertNotIn('Buy peacock feathers', page_text)
+		self.assertIn('Buy milk', page_text)
+		#Satisfied they both go back to sleep
+
+
+
 
 	def wait_for_row_in_list_table(self, row_text):
 		start_time = time.time()
